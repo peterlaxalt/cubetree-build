e88c17e45dce56c4e2c2356191836297121c67eb
peterlaxalt <pete@laxaltandmciver.co>
2016-05-09 13:12:56 -0700

    upload

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function headKey() {
+    if (document.getElementById("overlay").className = "idle") {
+      document.getElementById("overlay").className = "active";
+    } else if (document.getElementById("overlay").className = "active") {
+      document.getElementById("overlay").className = "idle";
+    }
+}
